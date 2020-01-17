f09bbc8e1e0d7459acc8e0dcbbe5fe09b296a555
Mouth <adrien.charneau@edu.itescia.fr>
2020-01-17 17:06:54 +0100

    enlevé l'affichage débug chelou

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -71,7 +71,7 @@ class _SignInState extends State<SignIn> {
                 ),
                 onPressed: () async {
                   if(_formKey.currentState.validate()){
-                    print('valid');
+                    //print('valid');
                     dynamic result = await _auth.signInWithEmailAndPassword(email, password);
                     if(result == null){
                       setState(() => error = 'Could not sign in with those credentials');
