f33ae810ce52a75364b3b43ce8a0ee3d17431119
Mouth <adrien.charneau@edu.itescia.fr>
2020-01-17 10:40:58 +0100

    ça part en vrille - implémentation du questionnaire

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase/services/auth.dart';
 
+//////////////////////////////////////////
+///
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState()
+  {
+    return HomeState();
+  }
+}
+///
+//////////////////////////////////////////
 
-class Home extends StatelessWidget {
+class HomeState extends State<Home> {
 
   final AuthService _auth = AuthService();
 
+//////////////////////////////////////////
+///
+  int questionIndex = 0;
+
+  var questions = 
+  [
+    'Color 0',
+    'Color 1',
+    'Color 2'
+  ];
+///
+//////////////////////////////////////////
+
   @override
   Widget build(BuildContext context) {
+
+//////////////////////////////////////////
+///
+    void answerQuestion()
+    {
+      print("color 0 chosen!");
+
+      if(questionIndex < 1)
+      {
+        setState(()
+        {
+          questionIndex += 1;
+          print(questionIndex);
+        });
+      }
+    }
+///
+/////////////////////////////////////////
     return Scaffold(
       backgroundColor: Colors.brown[50],
       appBar: AppBar(
@@ -25,6 +67,30 @@ class Home extends StatelessWidget {
           )
         ],
       ),
+
+/////////////////////////////////////////
+///
+        body: Column(
+          children: <Widget>[
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer 0'),
+              onPressed: answerQuestion
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: () => print("Color 1 chosen!")
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => {
+                print("Color 2 chosen!")
+              }
+            ),
+          ],
+        ),
+///
+////////////////////////////////////////
     );
   }
 }
